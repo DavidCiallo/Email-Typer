e40953813693becc1c88c076846823d99d9f29e7
DcolorWei <corfer@yeah.net>
2025-12-31 00:59:44 +0800

    refactor code by formatter and remove websocket

--- client/api/instance.ts
+++ client/api/instance.ts
@@ -1,9 +1,8 @@
 import { AuthRouterInstance } from "../../shared/router/AuthRouter";
-import { EmailRouterInstance, EmailWebsocketInstance } from "../../shared/router/EmailRouter";
+import { EmailRouterInstance } from "../../shared/router/EmailRouter";
 import { StrategyRouterInstance } from "../../shared/router/StrategyRouter";
-import { inject, injectws } from "../lib/inject";
+import { inject } from "../lib/inject";
 
 export const AuthRouter = new AuthRouterInstance(inject);
 export const EmailRouter = new EmailRouterInstance(inject);
-export const EmailWebsocket = new EmailWebsocketInstance(injectws);
 export const StrategyRouter = new StrategyRouterInstance(inject);
